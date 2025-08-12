04e4b6755113df107742cc2883c01f5683ea8942
TranDucKhiem14082005 <khiemtran015@gmail.com>
2025-08-12 23:53:58 +0700

    learn_nodejs

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const PgDatabase = require('./database/pg.database');
 const app = express();
 app.use(bodyParser.json());
 
-
+const pgDatabaseInstance = new PgDatabase();
+pgDatabaseInstance.connectDB();
 
 app.use('/v1/auth', require('./app/v1/routes'));
 
--- src/database/pg.database.js
+++ src/database/pg.database.js
@@ -9,20 +9,20 @@ class PgDatabase {
             user: dbConfigPG.user,
             password: dbConfigPG.password,
             database: dbConfigPG.database,
-            ssl: {
-                rejectUnauthorized: false // Set to true in production with proper SSL certificates
-            },
+            ssl: false,
             max: 20,
             idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
             connectionTimeoutMillis: 2000 // Return an error after 2 seconds if connection could not be established
         });
     }
 
-    async connect() {
+    async connectDB() {
         try{
-
+            const client = await this.pool.connect();
+            console.log("connect to database successfully");
+            client.release();
         }catch(error) {
-
+            console.log("Error connecting to the database: ",error);
         }
     }
 
